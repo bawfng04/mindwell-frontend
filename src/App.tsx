6a4ae144ed800f1eef92ac95c1c5fe6777fa0e74
bawfng04 <nguyendinhbang53az@gmail.com>
2025-12-08 13:48:34 +0700

    feat: add routing with react-router and create homepage component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Homepage.tsx";
 
 function App() {
-  return <>TEST</>;
+  return (
+    <Routes>
+      <Route path="/" element={Home()} />
+      <Route path="*" element={<div>404 Not Found</div>} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Trang chủ</h1>
+      <p>Chào mừng đến với MindWell!</p>
+      <>test - test - test</>
+    </div>
+  );
+};
+
+export default Home;
